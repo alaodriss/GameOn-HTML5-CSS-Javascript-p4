b61a698c3f1f79d7cfecea4a585230865056aeae
alaodriss <alaodriss@gmail.com>
2022-02-01 15:20:21 +0100

    version-1.4

--- js/form.js
+++ js/form.js
@@ -25,3 +25,49 @@ const validPrenom = function(inputPrenom){
   }
 };
 
+//////////////////////////////Nom////////////////////////////
+
+
+form.last.addEventListener('change',function(){
+  validNom(this);
+});
+
+const validNom = function(inputNom){
+
+  // Ceration de de la regxp pour valide le prenom;
+  let nomRegExp = new RegExp("^[a-zA-Z]{2,}$");
+  let testNom = nomRegExp.test(inputNom.value);
+
+  // recuperer la balise small
+  let small = inputNom.nextElementSibling;
+  // condition de verification 
+  if(testNom){
+    small.innerHTML= ''
+  }else {
+    small.innerHTML= '"Saisissez un nom qui contient au moins deux caractères alphabétiques";'
+  }
+};
+
+//////////////////////////////Mail////////////////////////////
+
+
+form.email.addEventListener('change',function(){
+  validEmail(this);
+});
+
+const validEmail = function(inputEmail){
+
+  // Ceration de de la regxp pour valide mail;
+  let emailRegExp = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+  let testEmail = emailRegExp.test(inputEmail.value);
+
+  // recuperer la balise small
+  let small = inputEmail.nextElementSibling;
+  // condition de verification 
+  if(testEmail){
+    small.innerHTML= ''
+  }else {
+    small.innerHTML= "Saisissez une adresse électronique valide";
+  }
+};
+
